04bd750ad2a11b5a3d0fb6857e8a89b7dd313288
Mario Reder <mario.reder@pm.me>
2026-01-12 05:14:28 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "The holy dex",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Rewards,Swap",
+  "VITE_ENABLED_MENUS": "Rewards,Swap,Trading,Leaderboard,Portfolio,Markets,Vaults",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
