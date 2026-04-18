1a5224d1d6148c7a1072ba22f3b1907d367337af
Mario Reder <mario.reder@pm.me>
2026-04-18 15:31:13 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/holylabseth",
   "VITE_SEO_SITE_NAME": "Holy Dex",
   "VITE_SEO_SITE_DESCRIPTION": "The holy dex",
-  "VITE_SEO_SITE_URL": "https://holylabs.xyz",
+  "VITE_SEO_SITE_URL": "https://dex.holylabs.xyz",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@HolyLabsETH",
